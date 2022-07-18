6babf37139070148bef06057aad47607e05e4042
Thomas Ruppeiner <truppeiner@gmail.com>
2022-07-18 17:02:50 -0500

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from 'react-dom';
 // eslint-disable-next-line
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
 
-const container = document.getElementById('root');
+ReactDOM.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
-const root = createRoot(container);
-
-root.render(<App></App>)
